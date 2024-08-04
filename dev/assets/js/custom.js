9120e3ae9981fda909af92935ef3b217c9891152
Maksym Peretiatko <maksym.peretiatko@lnu.edu.ua>
2024-08-04 20:17:42 +0300

    made information bar

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,1 +1,44 @@
-  
+const buildings = document.querySelectorAll(".building")
+
+const cellAdress = document.getElementById("adress")
+const cellFlatsAmount = document.getElementById("flats-amount")
+const cellFlatsFree = document.getElementById("flats-free")
+const cellFlatsBooked = document.getElementById("flats-booked")
+const cellFlatsSold = document.getElementById("flats-sold")
+
+function calcInfo() {
+    buildings.forEach(el => {
+        let flatsAmount = Number(el.getAttribute("data-flats-amount"))
+        let flatsBooked = Number(el.getAttribute("data-flats-booked"))
+        let flatsSold = Number(el.getAttribute("data-flats-sold"))
+
+        const flatsFree = flatsAmount - (flatsBooked + flatsSold)
+        
+        el.setAttribute("data-flats-free", flatsFree)
+        
+        if (flatsFree === 0) {
+            el.classList.add("sold")
+            el.addEventListener('click', (event)=> {
+                event.preventDefault();
+            })
+        }
+        
+    });
+}
+
+calcInfo()
+
+function getInfo(cell, attribute) {
+    buildings.forEach(el => {
+        el.addEventListener("mouseover", function() {
+            const value = el.getAttribute(attribute)
+            cell.innerHTML = value
+        })
+    });
+}
+
+getInfo(cellAdress, "data-adress")
+getInfo(cellFlatsAmount, "data-flats-amount")
+getInfo(cellFlatsFree, "data-flats-free")
+getInfo(cellFlatsBooked, "data-flats-booked")
+getInfo(cellFlatsSold, "data-flats-sold")
